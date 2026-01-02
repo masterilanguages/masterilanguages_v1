175bce4672da91c79d8a94c082d08b61dd5e3cef
marklewinmusic <marklewinmusic@gmail.com>
2026-01-02 01:00:50 +0000

    File changes

--- src/components/TranslatorWidget.jsx
+++ src/components/TranslatorWidget.jsx
@@ -217,7 +217,7 @@ Provide:
                   >
                     <span className="text-2xl">🎒</span>
                     {wordAdded && (
-                      <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl">✓</span>
+                      <span className="absolute inset-0 flex items-center justify-center text-3xl text-green-400">✓</span>
                     )}
                   </button>
                 )}
