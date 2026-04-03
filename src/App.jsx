b6a0d932cb390d9976d66775ef2a7160e26c6623
marklewinmusic <marklewinmusic@gmail.com>
2026-04-03 20:35:54 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
+import Session1Journal from '@/pages/Session1Journal';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
@@ -62,6 +63,7 @@ const AuthenticatedApp = () => {
           }
         />
       ))}
+      <Route path="/Session1Journal" element={<LayoutWrapper currentPageName="Session1Journal"><Session1Journal /></LayoutWrapper>} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
